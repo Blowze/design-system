6db1fa3daa42dbc98f5eace884439d1d5ac81d54
Алексей Крайнов <krainoff.alesha@ya.ru>
2019-10-30 15:16:19 +0300

    Исправил ряд багов

--- README.md
+++ README.md
@@ -2,20 +2,6 @@
 
 `viritsa-design-system` — это библиотека с открытым кодом, которая предоставляет набор готовых визуальных компонентов.
 
-## Блоки
-
-* [button](components/button/button.ru.md)
-
-## Поддерживаемые браузеры
-
-* **Desktop**
-  * Firefox 
-  * Chrome 
-  * Safari 
-  * Yandex 
-  * Opera 
-  * Internet Explorer 
-  
 ## Установка 
 
 ```bash
--- src/components/v-text/Text.vue
+++ src/components/v-text/Text.vue
@@ -1,26 +1,8 @@
 
 <script>
-    /**
-     * The only true button.
-     * @displayName Best Button
-     */
     import './Text.scss'
 
     export default {
         name: 'Text',
-        props: {
-            align: {
-                type: String,
-                default: '#333',
-                value: 'red'
-            },
-
-
-            size: {
-                type: String,
-                default: 'normal'
-            },
-        }
-      
     }
 </script>
