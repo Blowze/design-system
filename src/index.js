7cc5959db24545b01510d14b91e2dfa354a037d9
myashunin <yashunin.maskim@zolotoy.ru>
2020-01-14 11:09:39 +0300

    новый входной файл

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import FlxvIcon from "./elements/Icon/Icon.vue";
 
-export default {
-    install(Vue, options){
-        Vue.component("flxv-icon", FlxvIcon);
-    }
-
+// Declare install function executed by Vue.use()
+export function install(Vue) {
+	if (install.installed) return;
+	install.installed = true;
+	Vue.component("flxv-icon", FlxvIcon);
 }
+
+// Create module definition for Vue.use()
+const plugin = {
+	install,
+};
+
+// Auto-install when vue is found (eg. in browser via <script> tag)
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+	GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+	GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+	GlobalVue.use(plugin);
+}
+
+// To allow use as module (npm/webpack/etc.) export component
+export default FlxvIcon;
